c201824cec10077d8492cbdbc254d399dee1d0d4
Jesse <2637370247@qq.com>
2026-02-23 16:38:29 +0800

    complete database

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from web.models.user import UserProfile
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user',)
 
 # Register your models here.
