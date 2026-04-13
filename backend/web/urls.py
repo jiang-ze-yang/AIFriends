666de419bcf8679258199dee374754c3ca37b8e0
Jesse <2637370247@qq.com>
2026-04-13 19:06:15 +0800

    创建更新用户资料后端api

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -4,6 +4,7 @@ from web.views.user.account.logout import LogoutView
 from web.views.user.account.register import RegisterView
 from web.views.user.account.refresh_token import RefreshTokenView
 from web.views.user.account.get_user_info import GetUserInfoView
+from web.views.user.profile.update import UpdateProfileView
 from web.views.index import index
 
 urlpatterns = [
@@ -12,7 +13,8 @@ urlpatterns = [
     path("api/user/account/register/", RegisterView.as_view()),
     path("api/user/account/refresh_token/", RefreshTokenView.as_view()),
     path("api/user/account/get_user_info/", GetUserInfoView.as_view()),
-    
+
+    path('api/user/profile/update/', UpdateProfileView.as_view()),
     path('', index),
     
     re_path(r'^(?!media/|static/|assets/).*$', index)
