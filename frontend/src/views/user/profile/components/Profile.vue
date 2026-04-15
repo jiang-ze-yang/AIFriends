33efe9795a83c1391553d634d6f99e4b2c8d28d8
Jesse <2637370247@qq.com>
2026-04-15 08:10:20 +0800

    实现编辑资料页面前端

---
+++ frontend/src/views/user/profile/components/Profile.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref, watch } from 'vue';
+const props = defineProps(['profile'])
+const myProfile = ref(props.profile)
+
+watch(() => props.profile, newVal => {
+    myProfile.value = newVal
+})
+
+defineExpose({
+    myProfile,
+})
+</script>
+
+
+<template>
+    <fieldset class="fieldset">
+        <label class="label text-base">个人简介</label>
+        <textarea rows="6" class = "textarea w-108" v-model="myProfile"></textarea>
+    </fieldset>
+</template>
+
+<style scoped>
+</style> 
