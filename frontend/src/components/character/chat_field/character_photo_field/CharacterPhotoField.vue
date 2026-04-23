be84345ad907d3048bba6c0380ed0be78d13420f
Jesse <2637370247@qq.com>
2026-04-23 16:11:42 +0800

    实现聊天框

---
+++ frontend/src/components/character/chat_field/character_photo_field/CharacterPhotoField.vue
@@ -0,0 +1,19 @@
+<script setup>
+    defineProps(['character'])
+</script>
+
+<template>
+    <div class="absolute left-2 right-22 top-6 h-8 w-fit rounded-full bg-black/50 flex items-center gap-2 pr-3">
+        <div class="avatar">
+            <div class="w-8 h-8 rounded-full"> <img :src="character.photo" alt="avatar" class="object-cover w-full h-full">
+            </div>
+        </div>
+        <div class="text-white text-sm line-clamp-1 break-all">
+            {{ character.name }}    
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
